a817882e86e93d878e85438ec59ce44294a6e08f
Samuel Freeman <soundmaking@merfasmean.com>
2017-08-29 12:05:45 +0100

    moved prep_text_data() to sdfsys_gamma.py
    Signed-off-by: Samuel Freeman <soundmaking@merfasmean.com>

--- sdfsys_gamma.py
+++ sdfsys_gamma.py
@@ -77,3 +77,80 @@ class SysUi():
     infobox_size_x = 0.4
     infobox_size_y = 1-(bttn.y)
 
+class TextBuffer():
+
+    text_data = []
+    this_line = []
+    this_word = []
+
+    def prep_text_data(self, text):
+        print('parse text')
+
+        self.text_data = []
+
+        self.this_line = []
+        this_line_is_empty = True
+
+        self.this_word = []
+        this_word_is_empty = True
+
+        is_space = True
+        is_endline = False
+
+        for char in text:
+            ascii = ord(char)
+
+            if ascii is 10:
+                is_endline = True
+            else:
+                is_endline = False
+
+            if (ascii is 32) or (ascii is 9):
+                is_space = True
+            else:
+                is_space = False
+
+            if (not is_space) and (not is_endline):
+                # build up word until next space or end of line
+                self.this_word.append(char)
+                this_word_is_empty = False
+
+            if is_space and (not this_word_is_empty):
+                # reached a space after a word
+                # join word into a string and add it to line
+                self.this_line.append(''.join(self.this_word))
+                this_line_is_empty = False
+
+                # reset word
+                self.this_word = []
+                this_word_is_empty = True
+
+
+            if is_endline:
+                if (not this_word_is_empty):
+                    # join word into a string and add it to line
+                    self.this_line.append(''.join(self.this_word))
+                    this_line_is_empty = False
+
+                    # reset word
+                    self.this_word = []
+                    this_word_is_empty = True
+
+                if (not this_line_is_empty):
+                    self.text_data.append(self.this_line)
+                    # reset line
+                    self.this_line = []
+                    this_line_is_empty = True
+        # end for char in text
+
+        if (not this_word_is_empty):
+            # join word into a string and add it to line
+            self.this_line.append(''.join(self.this_word))
+            this_line_is_empty = False
+
+        if (not this_line_is_empty):
+            self.text_data.append(self.this_line)
+
+        print(self.text_data)
+
+    # end def prep_text_data(self, text)
