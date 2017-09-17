c308e46c51b2e8f41889bf2515e2960bb3e462c7
Samuel Freeman <soundmaking@merfasmean.com>
2017-09-17 22:43:38 +0100

    change from Vector2 to Point2 for has_been_put
    Signed-off-by: Samuel Freeman <soundmaking@merfasmean.com>

--- thisis_gamma.py
+++ thisis_gamma.py
@@ -29,7 +29,7 @@ to_end_block_comment = ['*/', '../']
 
 
 class Thisis:
-    has_been_put = {'x': Vector2(0.50, 0.50), 'z': Vector2(0.00, 0.00)}
+    has_been_put = {'x': Point2(0.50, 0.50), 'z': Point2(0.00, 0.00)}
 
     def parse_line(self, txt_in):
         # txt_in should be a pre-processed list of strings that make up a command line
@@ -64,7 +64,7 @@ class Thisis:
                 x = float(txt_in[3])
                 y = float(txt_in[4])
                 print("x,y = ", [x, y])
-                self.has_been_put[v_name] = Vector2(x, y)
+                self.has_been_put[v_name] = Point2(x, y)
                 print('has_been_put = ', self.has_been_put)
                 return ['/>', v_name]
             # end if put_type is 'at'
